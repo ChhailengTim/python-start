b613e718298816f55501034b9d471a3cd8e4f14d
CHHAILENG TIM <chhailengtim2018@gmail.com>
2023-06-09 23:56:05 +0700

    10%

---
+++ ask_weight.py
@@ -0,0 +1,3 @@
+weight = input('Enter your age: ')
+weight_kg = int(weight) * 0.45
+print(weight_kg)
---
+++ py_string.py
@@ -0,0 +1,11 @@
+course = ' Hello From cambodia '
+sentence = '''
+Dear sir!.
+Hello I'm chhaileng tim from cambodia.
+Nice to meet you.
+Thanks
+'''
+note = 'Welcome'
+print(course)
+print(sentence)
+print(len(note))
