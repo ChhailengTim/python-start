62b7372f5702e34cead22c2c8a3fb113b4fcbf55
CHHAILENG TIM <chhailengtim2018@gmail.com>
2023-06-12 21:08:52 +0700

    any

---
+++ name_len.py
@@ -0,0 +1,7 @@
+name = 'J'
+if len(name) < 3:
+    print('Name need at least more than 3 characters')
+elif len(name) > 50:
+    print('Name must be a maximum of 50 characters')
+else:
+    print('Name look good!')
