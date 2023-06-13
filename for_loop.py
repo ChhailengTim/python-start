9ad4a4004e5ad911c981c6fb6b0bb167b2526f28
CHHAILENG TIM <chhailengtim2018@gmail.com>
2023-06-13 22:51:09 +0700

    loop

---
+++ for_loop.py
@@ -0,0 +1,12 @@
+for item in ["Python", "Java", "C"]:
+    print(item)
+
+for i in range(10):
+    print(i)
+
+prices = [10, 20, 30]
+
+total = 0
+for price in prices:
+    total += price
+print(f"Total: {total}")
