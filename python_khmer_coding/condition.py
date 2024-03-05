29cf9502881666f5bbe11645f4e358b43aa79f3d
CHHAILENG TIM <chhailengtim2018@gmail.com>
2024-03-05 16:38:43 +0700

    learn from khmer coding

---
+++ python_khmer_coding/condition.py
@@ -0,0 +1,27 @@
+a = 10
+b = 20
+
+if a > b:
+    print('Haha')
+elif a < b:
+    print('hehe')
+else:
+    print("Welcome")
+
+
+
+
+
+a = 1
+
+if a > 0:
+    print("Hi user")
+elif a == 1:
+    print("Nice to meet you!")
+else:
+    print("Bye bye")
+
+
+
+score = 75.5
+print("Passed") if score >=75 else print("Failed")
---
+++ python_khmer_coding/hello.py
@@ -0,0 +1,11 @@
+a = 10 
+b = 10.5
+c = True
+d = False
+e = "Hello"
+f = 'Hi'
+
+print("a=", a, "b=", b, "c=", c, "d=", d, "e=", e, "f=", f)
+
+# The operation couldn’t be completed. Unable to locate a Java Runtime.
+# Please visit http://www.java.com for information on installing Java.
